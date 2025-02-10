f841f21cef5ed2fbf1542aae55f8c2b4413aea3b
aldojdg <aldojdg@gmail.com>
2025-02-10 17:44:24 -0300

    Subida final

--- ad-portfolio/src/components/projectsSection/ProjectsText.jsx
+++ ad-portfolio/src/components/projectsSection/ProjectsText.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ProjectsText = () => {
   return (
     <div className="flex flex-col items-center mt-[100px]">
-      <h2 className="text-6xl text-cyan mb-10">Projectos</h2>
+      <h2 className="text-6xl text-cyan mb-10">Proyectos</h2>
       <p className="text-lg text-center text-white">
         He trabajado en una variedad de proyectos en análisis y desarrollo,
         desde sitios web responsivos para pequeñas empresas hasta aplicaciones
